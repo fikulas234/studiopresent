20a3a19e95d7cbcba215152f640cb444c3dbbbe7
fikulas234 <trbojevicfilip96@gmail.com>
2022-04-20 15:07:41 +0200

    updated

---
+++ Filip_Trbojevic/app.js
@@ -0,0 +1,12 @@
+/* const links = document.getElementById("links");
+const burger = document.getElementsByClassName("burger-nav");
+
+function hamburgerNav() {
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
+
+burger.addEventListener("click", hamburgerNav); */

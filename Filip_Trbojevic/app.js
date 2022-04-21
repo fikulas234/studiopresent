f1e46672a07a5312c122e9cd63272475451c1098
fikulas234 <trbojevicfilip96@gmail.com>
2022-04-21 15:03:47 +0200

    added hamburger navigation

--- Filip_Trbojevic/app.js
+++ Filip_Trbojevic/app.js
@@ -1,11 +1,26 @@
-/* const links = document.getElementsByClassName("line");
-const burger = document.getElementsByClassName("burger-nav");
+const hamburger = document.querySelector(".burger-nav");
+const navMenu = document.querySelector(".float-menu");
 
-burgerNav = function() {
-    burger.toogleClass.toogle("is-active")
-    
-}
+const left = document.querySelector(".left-trigger")
+const right = document.querySelector(".right-trigger")
 
-addEventListener("click", burgerNav); */
+const pic1 = document.getElementById("i1")
+const pic2 = document.getElementById("i2")
+const pic3 = document.getElementById("i3")
+const pic4 = document.getElementById("i4")
+const pic5 = document.getElementById("i5")
+
+
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+})
+
+
+left.addEventListener("click", () => {
+    console.log("bla");
+})
+
+
 
 

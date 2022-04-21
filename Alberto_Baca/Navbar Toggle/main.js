ce5f37c57c34f050f52b82d2803264e1e70d597e
Alberto <paris7287@gmail.com>
2022-04-21 23:37:55 +0200

    update

---
+++ Alberto_Baca/Navbar Toggle/main.js
@@ -0,0 +1,8 @@
+const ham = document.querySelector('.header .nav-bar .ham');
+const nav = document.querySelector('.header .nav-bar nav');
+
+ham.addEventListener('click', () => {
+    nav.classList.toggle('nav-toggle');
+    ham.classList.toggle('close');
+
+});

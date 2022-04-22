5987d9e07fcd1e6c4801b2c26fa07c2b3c98e611
Alberto <paris7287@gmail.com>
2022-04-22 14:33:19 +0200

    updatez

--- Alberto_Baca/Praksa/main.js
+++ Alberto_Baca/Praksa/main.js
@@ -1,8 +1,24 @@
+//Global Variables
+
 const ham = document.querySelector('.header .nav-bar .ham');
 const nav = document.querySelector('.header .nav-bar nav');
+var counter = 1;
 
 ham.addEventListener('click', () => {
     nav.classList.toggle('nav-toggle');
     ham.classList.toggle('close');
 
 });
+
+
+
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+
+    if (counter > 4){
+        counter = 1;
+    }
+
+
+}, 5000);
